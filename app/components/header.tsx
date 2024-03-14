51317a6c5175c38ef14c1daeba03d39a17a9e58c
Peer Maute <peer.maute@haw-hamburg.de>
2024-03-14 09:21:52 +0100

    style(#2): add proper header design

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,15 +1,26 @@
 "use client";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 const Header = () => {
   const router = useRouter();
   return (
-    <header>
-      <h1
-        className="text-3xl text-center mt-9 mb-9 hover:cursor-pointer"
-        onClick={() => router.push("/")}
-      >
-        Unimusik Hamburg
-      </h1>
+    <header className="shadow-sm pt-4 pb-2 mb-4">
+      <div className="flex justify-center items-center">
+        <h1
+          className="text-2xl mr-4 font-semibold pb-1 hover:cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          Unimusik
+        </h1>
+        <Image
+          src={"/unimusik-logo.png"}
+          alt={"Profile Picture"}
+          width={150}
+          height={150}
+          onClick={() => router.push("/")}
+          className="hover:cursor-pointer"
+        />
+      </div>
     </header>
   );
 };
