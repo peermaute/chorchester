b892f8383a95715f845597795b0931c93afaffd1
peermaute <peer.mail@web.de>
2025-04-11 23:25:35 +0200

    current state

--- app/(logged-in)/profile/page.tsx
+++ app/(logged-in)/profile/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { FormEvent, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
