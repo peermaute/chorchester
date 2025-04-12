5f2a2fcc29f57f1f9940fd1221829136ffa80cf5
peermaute <peer.mail@web.de>
2025-04-12 17:54:04 +0200

    unimusik chorchester fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Unimusik",
+  title: "Chorchester",
   description: "Your choir and orchestra companion",
 };
 
