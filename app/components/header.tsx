56a8b61480e6d1ff0aad8dab5671db7964a21403
peermaute <peer.mail@web.de>
2025-04-14 12:34:57 +0200

    feat(#31): change look of header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,26 +1,30 @@
 "use client";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+
 const Header = () => {
   const router = useRouter();
   return (
-    <header className="shadow-sm pt-4 pb-2 mb-4">
-      <div className="flex justify-center items-center">
-        <h1
-          className="text-2xl mr-4 font-semibold pb-1 hover:cursor-pointer"
-          onClick={() => router.push("/")}
-        >
-          Chorchester
-        </h1>
-        <Image
-          src={"/unimusik-logo.png"}
-          alt={"Profile Picture"}
-          width={150}
-          height={150}
-          priority={true}
-          onClick={() => router.push("/")}
-          className="hover:cursor-pointer"
-        />
+    <header className="w-full bg-background border-b border-border">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center py-4">
+          <div
+            className="flex items-center space-x-4 transition-transform duration-300 hover:scale-[1.02] active:scale-[0.98]"
+            onClick={() => router.push("/")}
+          >
+            <Image
+              src={"/unimusik-logo.png"}
+              alt={"Unimusik Logo"}
+              width={70}
+              height={70}
+              priority={true}
+              className="hover:cursor-pointer rounded-lg"
+            />
+            <h1 className="text-xl font-extrabold text-foreground hover:cursor-pointer font-sans uppercase tracking-tight">
+              Chorchester
+            </h1>
+          </div>
+        </div>
       </div>
     </header>
   );
